8f2c5e681635b4e9b3e653c7cc4ed1590ae1ce6e
Russell Telen <rtelen@my.bcit.ca>
2021-02-13 18:51:31 -0800

    Initial set up of screens

---
+++ src/screens/Tab1/TrendingMusic.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const TrendingMusic = () => {
+  return (
+    <View>
+      <Text>Trending Music</Text>
+    </View>
+  );
+};
+
+export default TrendingMusic;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ src/screens/Tab2/ArtistSearchScreen.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+
+const ArtistSearchScreen = ({ navigation }) => {
+  const showDetail = () => {
+    navigation.navigate("ArtistDetailScreen");
+  };
+  return (
+    <View>
+      <TouchableOpacity onPress={() => showDetail()}>
+        <Text>ArtistSearchScreen</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default ArtistSearchScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
