2efdf6c506046c1f88f7b6f21957552eed00e753
Russell Telen <rtelen@my.bcit.ca>
2021-02-18 15:49:05 -0800

    Display Images in detail screen

--- src/screens/Tab2/ArtistDetailScreen.js
+++ src/screens/Tab2/ArtistDetailScreen.js
@@ -1,13 +1,30 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Image, FlatList } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
 const ArtistDetailScreen = ({ route }) => {
   const artist = route.params;
-
+  const [photos, setPhotos] = useState([
+    artist.strArtistFanart,
+    artist.strArtistFanart2,
+    artist.strArtistFanart3,
+  ]);
   return (
-    <View style={styles.container}>
-      <Text>{artist.strArtist}</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <FlatList
+        keyExtractor={(artist) => artist.idArtist}
+        data={photos}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => {
+          console.log(item);
+          return (
+            <Image source={{ uri: item }} style={{ width: 400, height: 200 }} />
+          );
+        }}
+      />
+      <Text style={styles.name}>{artist.strArtist}</Text>
+    </ScrollView>
   );
 };
 
@@ -16,8 +33,10 @@ export default ArtistDetailScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  name: {
+    textAlign: "center",
+    fontSize: 30,
+    margin: 10,
   },
 });
