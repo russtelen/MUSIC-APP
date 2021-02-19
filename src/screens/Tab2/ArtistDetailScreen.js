029f9fc3572c3571ffe25fde08af34de86187951
Russell Telen <rtelen@my.bcit.ca>
2021-02-18 16:07:13 -0800

    Display rest of details

--- src/screens/Tab2/ArtistDetailScreen.js
+++ src/screens/Tab2/ArtistDetailScreen.js
@@ -23,7 +23,59 @@ const ArtistDetailScreen = ({ route }) => {
           );
         }}
       />
-      <Text style={styles.name}>{artist.strArtist}</Text>
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          marginHorizontal: 50,
+          marginTop: 20,
+        }}
+      >
+        <Text style={styles.name}>{artist.strArtist}</Text>
+        {artist.strArtistLogo ? (
+          <Image
+            source={{ uri: artist.strArtistLogo }}
+            style={{ width: 100, height: 50 }}
+          />
+        ) : null}
+      </View>
+      <Text style={{ textAlign: "center", fontStyle: "italic" }}>
+        {artist.strGenre}
+      </Text>
+      <Text
+        style={{
+          textAlign: "center",
+          fontWeight: "bold",
+          fontSize: 30,
+          marginTop: 20,
+        }}
+      >
+        About
+      </Text>
+      <Text style={{ marginTop: 20, padding: 20 }}>
+        {artist.strBiographyEN}
+      </Text>
+      {artist.strWebsite ? (
+        <Text style={{ padding: 20 }}>Website: {artist.strWebsite}</Text>
+      ) : null}
+
+      <Text
+        style={{
+          textAlign: "center",
+          fontWeight: "bold",
+          fontSize: 30,
+          marginTop: 20,
+        }}
+      >
+        Bio
+      </Text>
+      <Text style={{ marginTop: 20, padding: 20 }}>
+        Country: {artist.strCountry}
+      </Text>
+      <Text style={{ padding: 20 }}>Gender: {artist.strGender}</Text>
+      {artist.intBornYear ? (
+        <Text style={{ padding: 20 }}>Year of birth: {artist.intBornYear}</Text>
+      ) : null}
     </ScrollView>
   );
 };
